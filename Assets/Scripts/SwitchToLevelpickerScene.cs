79aec5a9fa32e53c20e81009ed0e79972641b54b
Saleh Chaaban <inf3636@hs-worms.de>
2022-06-14 23:40:54 +0200

    moved files, WIP: switch scene on trackpad press

--- Assets/Scripts/SwitchToLevelpickerScene.cs
+++ Assets/Scripts/SwitchToLevelpickerScene.cs
@@ -1,30 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 using UnityEngine.SceneManagement;
 
+// attach it to a new GameObject ?
 public class SwitchToLevelpickerScene : MonoBehaviour
 {
+    private List<InputDevice> leftHandedDevices;
+    private InputDevice leftHandTargetDevice;
+
     /*
-    [SerializeField] private XRController leftHandController;
-    [SerializeField] private UnityEvent OnTriggerPressed;
+    // approach 1
+    void Update()
+    {
+        if (leftHandedDevices == null)
+        {
+            InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.LeftHand, leftHandedDevices);
+            Debug.Log("devices found = " + leftHandedDevices.Count);
+            foreach (var device in leftHandedDevices)
+                Debug.Log(string.Format("device name = " + device.name + " device role = " + device.characteristics));
+
+            if (leftHandedDevices.Count > 0)
+                leftHandTargetDevice = leftHandedDevices[0];
+        }
+
+        bool touchpadPressed;
+        // checking for a press on the trackpad
+        bool couldGetFeatureValue = leftHandTargetDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out touchpadPressed);
+
+        if (couldGetFeatureValue && touchpadPressed)
+            SceneManager.LoadScene(sceneName: "LevelpickerScene");
+    }
+    */
+
+    // approach 2
+    void Start()
+    {
+        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, leftHandedDevices);
+
+        foreach (var device in leftHandedDevices)
+            Debug.Log(string.Format("device name = " + device.name + " device role = " + device.characteristics));
+
+        if(leftHandedDevices.Count > 0)
+            leftHandTargetDevice = leftHandedDevices[0];
+    }
 
     void Update()
     {
-        bool buttonPressed;
-        // check for button press & assign to bool
-        //leftHandController.inputDevice.IsPressed(button, out buttonPressed);
-        leftHandController.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out buttonPressed);
-        if (buttonPressed)
-        {
-            SceneManager.LoadScene(sceneName: "LevelpickerScene");
-        }
+        bool touchpadPressed;
+        // checking for a press on the trackpad
+        bool couldGetFeatureValue = leftHandTargetDevice.TryGetFeatureValue(CommonUsages.primary2DAxisClick, out touchpadPressed);
 
-        if (currentInteractor.GetComponent<ActionBasedController>().activateAction.action.ReadValue<float>() > 0.5f)
-        {
-            Debug.Log("trigger pressed");
-        }
+        if (couldGetFeatureValue && touchpadPressed)
+            SceneManager.LoadScene(sceneName: "LevelpickerScene");
     }
-    */
 }
